7fa49abdbde1bbb1b6f10a403fb7e2058b25a4cc
Edwin Wiersma <eddie1707@live.nl>
2014-10-07 17:41:48 +0200

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Edwin Wiersma on 06/10/14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image:UIImage = UIImage(named: "Ace")
+    var isRed = true
+}
